2308c1a0073193017776baab7388e799dcfb5325
Lrd134 <larryc3200@gmail.com>
2021-03-20 17:33:14 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
     attr_accessor :name, :artist, :genre
     @@all = []
@@ -10,10 +11,10 @@ class Song
     end
     def self.new_by_filename(filename) # Michael Jackson - Black or White - pop.mp3
         filename = filename.chomp(".mp3").split(" ")
-        artist_name = []
-        artist_name << filename[0]
+        artist_names = []
+        artist_names << filename[0]
         until filename.shift == "-"
-            artist_name << filename[0] if filename[0] != "-"
+            artist_names << filename[0] if filename[0] != "-"
         end
         song_name = []
         song_name << filename[0]
@@ -23,8 +24,17 @@ class Song
         genre = []
         genre << filename[0]
         self.new(song_name.join(" "))
-        self.artist = Artist.new(artist_name.join(" "))
         
+        @artist = Artist.new(artist_names.join(" "))
+        
+        
+    end
+    def artist_name=(name)
+        if self.artist.nil?
+            self.artist = Artist.new(name)
+        else
+            self.artist.name = name
+        end
     end
 
 end
